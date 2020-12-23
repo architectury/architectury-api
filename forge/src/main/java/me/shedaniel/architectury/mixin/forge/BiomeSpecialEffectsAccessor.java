6117b11483cf2b3f090549d405d7eb5ac32bab02
shedaniel <daniel@shedaniel.me>
2020-12-23 16:57:32 +0800

    Fix license, remove unused mixin

--- forge/src/main/java/me/shedaniel/architectury/mixin/forge/BiomeSpecialEffectsAccessor.java
+++
@@ -1,8 +0,0 @@
-package me.shedaniel.architectury.mixin.forge;
-
-import net.minecraft.world.level.biome.BiomeSpecialEffects;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(BiomeSpecialEffects.class)
-public interface BiomeSpecialEffectsAccessor {
-}
