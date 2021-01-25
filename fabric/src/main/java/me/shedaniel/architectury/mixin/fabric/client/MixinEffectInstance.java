bb33552a4965f10019c30fb24968bf56c6d309f0
shedaniel <daniel@shedaniel.me>
2021-01-25 17:33:10 +0800

    Fix #25

--- fabric/src/main/java/me/shedaniel/architectury/mixin/fabric/client/MixinEffectInstance.java
+++ fabric/src/main/java/me/shedaniel/architectury/mixin/fabric/client/MixinEffectInstance.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of architectury.
+ * Copyright (C) 2020, 2021 shedaniel
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package me.shedaniel.architectury.mixin.fabric.client;
 
 import com.mojang.blaze3d.shaders.Program;
@@ -10,7 +29,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Unique
-@Mixin(EffectInstance.class)
+@Mixin(value = EffectInstance.class, priority = 1050)
 public class MixinEffectInstance {
     @Redirect(
             method = "<init>",
