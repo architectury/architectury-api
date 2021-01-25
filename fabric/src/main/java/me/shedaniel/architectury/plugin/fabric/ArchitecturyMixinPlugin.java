a3a5307b34430a436dce91f37d7a7d54e5df9a35
Max <maxh2709@gmail.com>
2021-01-25 17:44:15 +0100

    (Hopefully) solve things with a mixin plugin
    Might be overkill, but we could need it again in the future...

---
+++ fabric/src/main/java/me/shedaniel/architectury/plugin/fabric/ArchitecturyMixinPlugin.java
@@ -0,0 +1,52 @@
+package me.shedaniel.architectury.plugin.fabric;
+
+import me.shedaniel.architectury.platform.Platform;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class ArchitecturyMixinPlugin implements IMixinConfigPlugin {
+    
+    @Override
+    public void onLoad(String mixinPackage) {
+        // noop
+    }
+    
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+    
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        switch (mixinClassName) {
+            case "me.shedaniel.architectury.mixin.fabric.client.MixinEffectInstance":
+                return !Platform.isModLoaded("satin");
+            default:
+                return true;
+        }
+    }
+    
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+        // noop
+    }
+    
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+    
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        // noop
+    }
+    
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        // noop
+    }
+}
