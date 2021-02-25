c8aeff08238768196751f675b5e2f7f1047b68dc
shedaniel <daniel@shedaniel.me>
2021-02-26 02:11:44 +0800

    Update architectury-plugin to 3.0.88

--- testmod-common/src/main/java/me/shedaniel/architectury/test/TestMod.java
+++ testmod-common/src/main/java/me/shedaniel/architectury/test/TestMod.java
@@ -21,9 +21,9 @@ package me.shedaniel.architectury.test;
 
 import me.shedaniel.architectury.platform.Platform;
 import me.shedaniel.architectury.test.debug.ConsoleMessageSink;
-import me.shedaniel.architectury.test.events.DebugEvents;
 import me.shedaniel.architectury.test.debug.MessageSink;
 import me.shedaniel.architectury.test.debug.client.ClientOverlayMessageSink;
+import me.shedaniel.architectury.test.events.DebugEvents;
 import me.shedaniel.architectury.test.gamerule.TestGameRules;
 import me.shedaniel.architectury.test.registry.TestRegistries;
 import me.shedaniel.architectury.test.registry.client.TestKeybinds;
