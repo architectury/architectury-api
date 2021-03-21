2751d844864ea2bf55c3b1b2ac9ad7f8961958b8
Max <maxh2709@gmail.com>
2021-03-21 20:19:37 +0100

    Add @Unique

--- fabric/src/main/java/me/shedaniel/architectury/mixin/fabric/MixinEntityCollisionContext.java
+++ fabric/src/main/java/me/shedaniel/architectury/mixin/fabric/MixinEntityCollisionContext.java
@@ -6,6 +6,7 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,18 +14,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityCollisionContext.class)
 public abstract class MixinEntityCollisionContext implements CollisionContext, EntityHooksImpl.CollisionContextExtension {
     
-    private Entity arch$entity = null;
+    @Unique
+    private Entity entity = null;
     
     @Inject(method = "<init>(Lnet/minecraft/world/entity/Entity;)V",
             at = @At("RETURN"))
     public void saveEntity(Entity entity, CallbackInfo ci) {
-        arch$entity = entity;
+        this.entity = entity;
     }
     
     @Nullable
     @Override
     public Entity getEntity() {
-        return arch$entity;
+        return entity;
     }
     
 }
