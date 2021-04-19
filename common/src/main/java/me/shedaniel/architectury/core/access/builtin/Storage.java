21769139b4bbfe542818c6c7f081f0a3c97cf1c7
shedaniel <daniel@shedaniel.me>
2021-04-19 15:39:24 +0800

    Add a little more javadocs

--- common/src/main/java/me/shedaniel/architectury/core/access/builtin/Storage.java
+++ common/src/main/java/me/shedaniel/architectury/core/access/builtin/Storage.java
@@ -6,6 +6,13 @@ import me.shedaniel.architectury.core.access.builtin.util.EmptyStorage;
 import java.util.Collection;
 
 public interface Storage<T> {
+    /**
+     * Returns a combined {@link Storage} from a collection of storages.
+     *
+     * @param storages the collection of storages, storages will be evaluated by its order.
+     * @param <T>      the type of storage
+     * @return the combined storage
+     */
     static <T> Storage<T> combine(Collection<Storage<T>> storages) {
         if (storages.size() == 0) return EmptyStorage.empty();
         if (storages.size() == 1) return storages.iterator().next();
@@ -22,5 +29,13 @@ public interface Storage<T> {
      */
     long extract(T type, long maxAmount, Transaction transaction);
     
+    /**
+     * Inserts {@link T} into the storage,
+     *
+     * @param type        the type to insert
+     * @param amount      the amount to insert
+     * @param transaction the transaction of this insertion
+     * @return the amount inserted
+     */
     long insert(T type, long amount, Transaction transaction);
 }
