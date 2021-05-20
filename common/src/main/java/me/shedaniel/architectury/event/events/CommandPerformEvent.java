72645f452c246a581badc7d2f613f8704c901218
canitzp <12819060+canitzp@users.noreply.github.com>
2021-05-20 21:59:00 +0200

    Update common/src/main/java/me/shedaniel/architectury/event/events/CommandPerformEvent.java
    Co-authored-by: Juuxel <6596629+Juuxel@users.noreply.github.com>

--- common/src/main/java/me/shedaniel/architectury/event/events/CommandPerformEvent.java
+++ common/src/main/java/me/shedaniel/architectury/event/events/CommandPerformEvent.java
@@ -33,7 +33,7 @@ import org.jetbrains.annotations.Nullable;
  * <p>A command fails when any other result than {@link net.minecraft.world.InteractionResult#PASS} is returned.
  * When PASS is used the {@link CommandPerformEvent#getResults()} is used for execution.
  * 
- * This is equal to the forge {@code CommandEvent} event.
+ * <p>This is equal to the Forge {@code CommandEvent} event.
  */
 public class CommandPerformEvent {
     public static final Event<Actor<CommandPerformEvent>> EVENT = EventFactory.createActorLoop();
