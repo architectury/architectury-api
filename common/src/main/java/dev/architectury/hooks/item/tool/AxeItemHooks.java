c8f790448142d60b43f1a6869710507c68fd65c1
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-08-11 15:51:32 +0300

    Validate stripping recipe before adding

--- common/src/main/java/dev/architectury/hooks/item/tool/AxeItemHooks.java
+++ common/src/main/java/dev/architectury/hooks/item/tool/AxeItemHooks.java
@@ -3,6 +3,7 @@ package dev.architectury.hooks.item.tool;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 
 import java.util.HashMap;
 
@@ -11,6 +12,10 @@ public final class AxeItemHooks {
     }
 
     public static void addStrippingRecipe(Block src, Block dst) {
+        if (!src.defaultBlockState().hasProperty(RotatedPillarBlock.AXIS))
+            throw new IllegalArgumentException("Source block is missing required 'AXIS' property!");
+        if (!dst.defaultBlockState().hasProperty(RotatedPillarBlock.AXIS))
+            throw new IllegalArgumentException("Destination block is missing required 'AXIS' property!");
         if (AxeItem.STRIPPABLES instanceof ImmutableMap) {
             AxeItem.STRIPPABLES = new HashMap<>(AxeItem.STRIPPABLES);
         }
