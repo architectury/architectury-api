5d276f04ed924b3b995d2cf01aef6024f9d62b9e
Max <maxh2709@gmail.com>
2022-02-19 00:43:12 +0800

    [ci skip] Some cleanup for Platform class and other platform-specific stuff (#201)
    * Ensure paths returned by Platform are absolute, add javadocs to Platform
    
    Signed-off-by: Max <maxh2709@gmail.com>
    
    * Use putIfAbsent for event buses to clean up some minor nastyness
    
    Signed-off-by: Max <maxh2709@gmail.com>
    
    * Remove explicit NotNull annotation

--- forge/src/main/java/me/shedaniel/architectury/platform/forge/PlatformImpl.java
+++ forge/src/main/java/me/shedaniel/architectury/platform/forge/PlatformImpl.java
@@ -91,11 +91,11 @@ public class PlatformImpl {
         private final ModInfo info;
         
         public ModImpl(String id) {
-            this.container = ModList.get().getModContainerById(id).get();
+            this.container = ModList.get().getModContainerById(id).orElseThrow();
             this.info = ModList.get().getMods().stream()
                     .filter(modInfo -> Objects.equals(modInfo.getModId(), id))
                     .findAny()
-                    .get();
+                    .orElseThrow();
         }
         
         @Override
