0aac2d856decba53a736dfa2849f9aefb6a3c6c9
shedaniel <daniel@shedaniel.me>
2022-05-02 21:17:44 +0800

    Add bucket item wrapper and add test mod

---
+++ forge/src/main/java/dev/architectury/core/item/forge/imitator/ArchitecturyBucketItem.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of architectury.
+ * Copyright (C) 2020, 2021, 2022 architectury
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package dev.architectury.core.item.forge.imitator;
+
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.world.item.BucketItem;
+import net.minecraft.world.level.material.Fluid;
+
+public class ArchitecturyBucketItem extends BucketItem {
+    public ArchitecturyBucketItem(RegistrySupplier<Fluid> fluid, Properties properties) {
+        super(fluid, properties);
+    }
+    
+    public final Fluid getContainingFluid() {
+        return getFluid();
+    }
+}
