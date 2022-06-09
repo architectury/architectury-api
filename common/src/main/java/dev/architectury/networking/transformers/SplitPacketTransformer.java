796ee4fc1413aa72f9863348c50a2078d71da9e3
shedaniel <daniel@shedaniel.me>
2022-06-09 19:16:14 +0800

    Fix SplitPacketTransformer not saving enough space for packet id (#273)

--- common/src/main/java/dev/architectury/networking/transformers/SplitPacketTransformer.java
+++ common/src/main/java/dev/architectury/networking/transformers/SplitPacketTransformer.java
@@ -35,6 +35,7 @@ import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 @ApiStatus.Experimental
@@ -179,7 +180,7 @@ public class SplitPacketTransformer implements PacketTransformer {
     
     @Override
     public void outbound(NetworkManager.Side side, ResourceLocation id, FriendlyByteBuf buf, TransformationSink sink) {
-        int maxSize = (side == NetworkManager.Side.C2S ? 32767 : 1048576) - 1 - 20;
+        int maxSize = (side == NetworkManager.Side.C2S ? 32767 : 1048576) - 1 - 20 - id.toString().getBytes(StandardCharsets.UTF_8).length;
         if (buf.readableBytes() <= maxSize) {
             ByteBuf stateBuf = Unpooled.buffer(1);
             stateBuf.writeByte(ONLY);
