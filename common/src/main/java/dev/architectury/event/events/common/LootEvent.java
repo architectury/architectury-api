635e1e6a9340ae9d87571af29259baf731e248c4
Juuxel <6596629+Juuxel@users.noreply.github.com>
2022-06-28 03:34:36 +0300

    Add loot table modification event
    Closes #42. It's a simple wrapper around the platform events.

---
+++ common/src/main/java/dev/architectury/event/events/common/LootEvent.java
@@ -0,0 +1,87 @@
+/*
+ * This file is part of architectury.
+ * Copyright (C) 2020, 2021, 2022 architectury
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package dev.architectury.event.events.common;
+
+import dev.architectury.event.Event;
+import dev.architectury.event.EventFactory;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTables;
+import org.jetbrains.annotations.ApiStatus;
+
+/**
+ * Events related to loot tables and loot generation.
+ */
+public interface LootEvent {
+    /**
+     * An event to modify loot tables when they are loaded.
+     * This can be used to add new drops via new loot pools to existing loot tables
+     * without replacing the entire table.
+     *
+     * <h2>Example: adding diamonds as a drop for dirt</h2>
+     * <pre>{@code
+     * LootEvent.MODIFY_LOOT_TABLE.register((lootTables, id, context) -> {
+     *     // Check that the loot table is dirt
+     *     if (Blocks.DIRT.getLootTable().equals(id)) {
+     *         // Create a loot pool with a single item entry of Items.DIAMOND
+     *         LootPool.Builder pool = LootPool.lootPool().add(LootItem.lootTableItem(Items.DIAMOND));
+     *         context.addPool(pool);
+     *     }
+     * });
+     * }</pre>
+     *
+     * @see ModifyLootTable#modifyLootTable(LootTables, ResourceLocation, LootTableModificationContext)
+     */
+    Event<ModifyLootTable> MODIFY_LOOT_TABLE = EventFactory.createLoop();
+    
+    @FunctionalInterface
+    interface ModifyLootTable {
+        /**
+         * Modifies a loot table.
+         *
+         * @param lootTables the {@link LootTables} instance containing all loot tables
+         * @param id         the loot table ID
+         * @param context    the context used to modify the loot table
+         */
+        void modifyLootTable(LootTables lootTables, ResourceLocation id, LootTableModificationContext context);
+    }
+    
+    /**
+     * A platform-specific bridge for modifying a specific loot table.
+     */
+    @ApiStatus.NonExtendable
+    interface LootTableModificationContext {
+        /**
+         * Adds a pool to the loot table.
+         *
+         * @param pool the pool to add
+         */
+        void addPool(LootPool pool);
+        
+        /**
+         * Adds a pool to the loot table.
+         *
+         * @param pool the pool to add
+         */
+        default void addPool(LootPool.Builder pool) {
+            addPool(pool.build());
+        }
+    }
+}
---
+++ fabric/src/main/java/dev/architectury/event/fabric/LootTableModificationContextImpl.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of architectury.
+ * Copyright (C) 2020, 2021, 2022 architectury
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package dev.architectury.event.fabric;
+
+import dev.architectury.event.events.common.LootEvent;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+
+final class LootTableModificationContextImpl implements LootEvent.LootTableModificationContext {
+    private final LootTable.Builder tableBuilder;
+    
+    LootTableModificationContextImpl(LootTable.Builder tableBuilder) {
+        this.tableBuilder = tableBuilder;
+    }
+    
+    @Override
+    public void addPool(LootPool pool) {
+        tableBuilder.pool(pool);
+    }
+    
+    @Override
+    public void addPool(LootPool.Builder pool) {
+        tableBuilder.withPool(pool);
+    }
+}
