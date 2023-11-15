cc2b8c748911c015ce2804e2f9b21e7735bf0359
shedaniel <daniel@shedaniel.me>
2023-11-15 20:06:46 +0800

    Fix #448

--- forge/src/main/java/dev/architectury/hooks/level/entity/forge/PlayerHooksImpl.java
+++ forge/src/main/java/dev/architectury/hooks/level/entity/forge/PlayerHooksImpl.java
@@ -19,11 +19,11 @@
 
 package dev.architectury.hooks.level.entity.forge;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.common.util.FakePlayer;
 
 public class PlayerHooksImpl {
     public static boolean isFake(Player playerEntity) {
-        return playerEntity instanceof FakePlayer;
+        return playerEntity instanceof ServerPlayer && playerEntity.getClass() != ServerPlayer.class;
     }
 }
