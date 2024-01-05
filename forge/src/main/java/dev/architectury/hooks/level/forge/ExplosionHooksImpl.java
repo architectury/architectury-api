dc2639835afea6c42304218c22902b479bc0b8f0
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-01-05 12:56:09 +1100

    fix explosion hooks

--- forge/src/main/java/dev/architectury/hooks/level/forge/ExplosionHooksImpl.java
+++ forge/src/main/java/dev/architectury/hooks/level/forge/ExplosionHooksImpl.java
@@ -24,6 +24,6 @@ import net.minecraft.world.phys.Vec3;
 
 public class ExplosionHooksImpl {
     public static Vec3 getPosition(Explosion explosion) {
-        return explosion.getPosition();
+        return explosion.center();
     }
 }
