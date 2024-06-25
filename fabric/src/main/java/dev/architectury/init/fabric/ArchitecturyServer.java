c0aabddad98aebca6a0dfbc3cd84bc9f5a3e1795
Nico Mexis <nico.mexis@kabelmail.de>
2024-06-25 14:07:35 +0200

    Register SpawnEntityPacket on Fabric server

--- fabric/src/main/java/dev/architectury/init/fabric/ArchitecturyServer.java
+++ fabric/src/main/java/dev/architectury/init/fabric/ArchitecturyServer.java
@@ -20,9 +20,12 @@
 package dev.architectury.init.fabric;
 
 import dev.architectury.event.events.common.LifecycleEvent;
+import dev.architectury.networking.SpawnEntityPacket;
 
 public class ArchitecturyServer {
     public static void init() {
         LifecycleEvent.SETUP.invoker().run();
+        
+        SpawnEntityPacket.register();
     }
 }
