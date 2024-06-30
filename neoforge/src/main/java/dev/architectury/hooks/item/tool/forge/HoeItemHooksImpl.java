7e5dcce3c8fc294febb3a553fb479268bcc520f9
Nico Mexis <nico.mexis@kabelmail.de>
2024-06-30 16:16:40 +0200

    Fix NeoForge 21.0.40+

--- neoforge/src/main/java/dev/architectury/hooks/item/tool/forge/HoeItemHooksImpl.java
+++ neoforge/src/main/java/dev/architectury/hooks/item/tool/forge/HoeItemHooksImpl.java
@@ -22,8 +22,8 @@ package dev.architectury.hooks.item.tool.forge;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.ItemAbilities;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.common.ToolActions;
 import net.neoforged.neoforge.event.level.BlockEvent;
 
 import java.util.function.Consumer;
@@ -34,7 +34,7 @@ public class HoeItemHooksImpl {
     public static void addTillable(Block input, Predicate<UseOnContext> predicate, Consumer<UseOnContext> action, Function<UseOnContext, BlockState> function) {
         NeoForge.EVENT_BUS.<BlockEvent.BlockToolModificationEvent>addListener(event -> {
             UseOnContext context = event.getContext();
-            if (ToolActions.HOE_TILL == event.getToolAction() && context.getItemInHand().canPerformAction(ToolActions.HOE_TILL)
+            if (ItemAbilities.HOE_TILL == event.getItemAbility() && context.getItemInHand().canPerformAction(ItemAbilities.HOE_TILL)
                     && event.getState().is(input) && predicate.test(context)) {
                 if (!event.isSimulated()) {
                     action.accept(context);
