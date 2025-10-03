2c0eb1b39032c2610c7ba04980afbfb46643ad30
Kevin <66999025+DAQEM@users.noreply.github.com>
2025-10-03 17:13:52 +0200

    fix: change keyCode to action
    The int passed to the keyPressed function represents the action instead of the keyCode. The keyCode is inside the keyEvent. The action is 1 for key pressed and 0 for key released.

--- common/src/main/java/dev/architectury/event/events/client/ClientRawInputEvent.java
+++ common/src/main/java/dev/architectury/event/events/client/ClientRawInputEvent.java
@@ -47,12 +47,12 @@ public interface ClientRawInputEvent {
          * Equivalent to Forge's {@code InputEvent.KeyInputEvent} event.
          *
          * @param client   The Minecraft instance performing it.
-         * @param keyCode  The key code.
+         * @param action   The action that should be performed.
          * @param keyEvent The key event.
          * @return A {@link EventResult} determining the outcome of the event,
          * the execution of the vanilla pressing mechanism may be cancelled by the result.
          */
-        EventResult keyPressed(Minecraft client, int keyCode, KeyEvent keyEvent);
+        EventResult keyPressed(Minecraft client, int action, KeyEvent keyEvent);
     }
     
     interface MouseScrolled {
